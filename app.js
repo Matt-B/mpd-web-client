bd42e4fef8097addb449ac0ec78f3e657ad78973
Matt B <matt@thebaileyresidence.co.uk>
2012-12-29 17:29:15 +0000

    Added socket.io so that whenever MPD issues a change event, all clients are notified and the view is reloaded for each client.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path'),
-    mpd = require('./mpd');
-
-var app = express();
+    mpd = require('./mpd'),
+    client = mpd.getClient(),
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -31,6 +33,15 @@ app.post('/next', routes.next);
 app.post('/previous', routes.previous);
 app.post('/setvol', routes.setvol);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log('A client connected...');
+});
+
+client.on('changed', function(player) {
+  console.log('change happening, broadcasting...');
+  io.sockets.emit('change', { item: player });
 });
