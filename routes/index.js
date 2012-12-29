c185714e834580460a6f2ca10304b08f7170358b
Matt B <matt@thebaileyresidence.co.uk>
2012-12-29 15:10:43 +0000

    Changed to a single connection, shared between all users of the app. I think this will be necessary in order to watch for updates from MPD.

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,50 @@
 var komponist = require('komponist'),
     hostname = '192.168.5.6';
+    var client = komponist.createConnection(6600, hostname, function(err) {
+      if(err)
+        console.log(err);
+    });
 
 
-exports.index = function(req, res){
-  var client = komponist.createConnection(6600, hostname, function() {
-    client.currentsong(function(err, info) {
-      client.status(function(err, status) {
-        client.listall(function(err, list) {
-          var fileList = [];
-          for(i=0; i < list.length; i++) {
-            if(list[i].file)
-              fileList.push(list[i]);
-          }
-          res.render('index', {title: 'MPD Status', status: status, error: err, info: info, list: fileList });
-          client.command('close');
-        });        
-      });          
-    });  
-  });
+exports.index = function(req, res){  
+  client.currentsong(function(err, info) {
+    client.status(function(err, status) {
+      client.listall(function(err, list) {
+        var fileList = [];
+        for(i=0; i < list.length; i++) {
+          if(list[i].directory)
+            fileList.push(list[i]);
+        }
+        res.render('index', {title: 'MPD Status', status: status, error: err, info: info, list: fileList });
+      });        
+    });          
+  });  
 };
 
 exports.toggle = function(req, res){
-  var client = komponist.createConnection(6600, hostname, function() {
-    client.toggle(function(err) {
-      res.redirect('/');
-      client.command('close');
-    });
+  client.toggle(function(err) {
+    res.redirect('/');
   });
 };
 
 exports.next = function(req, res){
-  var client = komponist.createConnection(6600, hostname, function() {
-    client.next(function(err) {
-      res.redirect('/');
-      client.command('close');
-    });
+  client.next(function(err) {
+    res.redirect('/');
   });
 };
 
 exports.previous = function(req, res){
-  var client = komponist.createConnection(6600, hostname, function() {
-    client.previous(function(err) {
-      res.redirect('/');
-      client.command('close');
-    });
+  client.previous(function(err) {
+    res.redirect('/');
   });
 };
 
 exports.setvol = function(req, res){
-  var client = komponist.createConnection(6600, hostname, function() {
-    client.setvol(req.param('volume'), function(err) {
-      res.redirect('/');
-      client.command('close');
-    });
+  client.setvol(req.param('volume'), function(err) {
+    res.redirect('/');
   });
 };
+
+client.on('changed', function(system) {     
+     console.log('Something has happened: '+ system);
+});
