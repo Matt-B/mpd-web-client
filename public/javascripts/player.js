eb9a62a37e9f656c3c0a5b5efaaa15254947536a
Matt B <matt@thebaileyresidence.co.uk>
2013-01-01 21:11:33 +0000

    Added client-side javascript to make most controls work. Only things not working now are the playlist and the track search sections.

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -6,5 +6,8 @@ var socket = io.connect('http://localhost:3000');
 
 function dispatchCommand(command, param) {
   console.log("Command: "+command+" called with param "+param);
+  $.post('/'+command, param, function(response) {
+      // log the response to the console
+      console.log("Response: "+response);
+  });  
 }
-
