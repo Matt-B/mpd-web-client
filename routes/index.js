e16f5615d1467d187bb9496544b460cb91e92094
Matt B <matt@thebaileyresidence.co.uk>
2013-01-01 22:37:30 +0000

    Playlist now displays correctly. Search still not working, yet.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,8 @@ var client = require('../mpd').getClient();
 exports.index = function(req, res){
   client.currentsong(function(err, info) {
     client.status(function(err, status) {
-      client.listall(function(err, list) {
-        var fileList = [];
-        for(i=0; i < list.length; i++) {
-          if(list[i].directory)
-            fileList.push(list[i]);
-        }
-        res.render('index', {title: 'MPD Status', status: status, error: err, info: info, list: fileList });
+      client.playlistinfo(function(err, playlist) {        
+        res.render('index', {title: 'MPD Status', status: status, error: err, info: info, playlist: playlist });
       });        
     });          
   });  
