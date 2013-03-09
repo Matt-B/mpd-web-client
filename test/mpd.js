df4b994a4457b851ca110be8bcd2684327935fde
Matt B <matt@thebaileyresidence.co.uk>
2013-03-09 01:05:24 +0000

    Add failing test for volume issue.

--- test/mpd.js
+++ test/mpd.js
@@ -46,4 +46,38 @@ describe('mpd', function(){
     mpd.pause();
     spy.should.have.been.not_called;
   });
+
+  it('should not try and set volume when mpd reports volume is N/A', function(){
+    var spy = chai.spy();
+    mpd.__set__({
+      "isConnected": true,
+      "komponist": {
+        setvol: function(){
+          spy();
+        },
+        status: function(){
+          return {
+            volume: '-1',
+            repeat: '0',
+            random: '0',
+            single: '0',
+            consume: '0',
+            playlist: '2',
+            playlistlength: '2',
+            xfade: '0',
+            mixrampdb: '0.000000',
+            mixrampdelay: 'nan',
+            state: 'pause',
+            song: '1',
+            songid: '1',
+            time: '14:250',
+            elapsed: '14.000',
+            bitrate: '0',
+            audio: '44100:24:2' };
+        }
+      }
+    });
+    mpd.setVolume(20);
+    spy.should.have.been.not_called;
+  });
 });
